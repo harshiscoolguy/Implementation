5659ba78ffe10f6bb9538fe5687178532c8a4cb5
Harsh Patel <harshiscoolguy@gmail.com>
2014-04-23 12:01:11 -0500

    Hello

---
+++ DeveloperTestRunner/FindNthPrimeTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeveloperTestRunner
+{
+    internal class FindNthPrimeTests : Tests
+    {
+        public void FindNthPrimeTest1()
+        {
+            Tester.AreEqual(Implementation.FindNthPrimeNumber(5), 11);
+        }
+
+        public void FindNthPrimeTest2()
+        {
+            Tester.AreEqual(Implementation.FindNthPrimeNumber(10), 29);
+        }
+    }
+}
